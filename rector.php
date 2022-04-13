4b9837410f4b1f2002693d94a0c4c483abf37e7f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-13 14:36:24 +0700

    [DX] Use RectorConfig instead of ContainerConfigurator (#15)

--- rector.php
+++ rector.php
@@ -2,22 +2,25 @@
 
 declare(strict_types=1);
 
+use Rector\Config\RectorConfig;
+
 use Rector\Core\Configuration\Option;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::DEAD_CODE);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::DEAD_CODE);
+    $rectorConfig->import(SetList::NAMING);
 
-    $parameters = $containerConfigurator->parameters();
+    $parameters = $rectorConfig->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $parameters->set(Option::SKIP, [
+        StringClassNameToClassConstantRector::class => [__DIR__ . '/config'],
     ]);
 
     $parameters->set(Option::PARALLEL, true);
--- tests/Rector/StaticCall/AddRemovedDefaultValuesRector/config/configured_rule.php
+++ tests/Rector/StaticCall/AddRemovedDefaultValuesRector/config/configured_rule.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\PHPOffice\Rector\StaticCall\AddRemovedDefaultValuesRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Config\RectorConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
+use Rector\PHPOffice\Rector\StaticCall\AddRemovedDefaultValuesRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $services = $rectorConfig->services();
     $services->set(AddRemovedDefaultValuesRector::class);
 };
