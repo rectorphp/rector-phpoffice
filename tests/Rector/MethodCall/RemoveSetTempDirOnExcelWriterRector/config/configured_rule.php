f78074aaa82074d65849194ec070eb990816408a
Tomas Votruba <tomas.vot@gmail.com>
2022-04-23 13:40:47 +0200

    spaces

--- tests/Rector/MethodCall/RemoveSetTempDirOnExcelWriterRector/config/configured_rule.php
+++ tests/Rector/MethodCall/RemoveSetTempDirOnExcelWriterRector/config/configured_rule.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use Rector\PHPOffice\Rector\MethodCall\RemoveSetTempDirOnExcelWriterRector;
 
 return static function (RectorConfig $rectorConfig): void {
