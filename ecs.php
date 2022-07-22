ad7cfce089576330f59a5512768d7eb43a4bd55e
Tomas Votruba <tomas.vot@gmail.com>
2022-07-22 21:01:00 +0200

    cs

--- ecs.php
+++ ecs.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
